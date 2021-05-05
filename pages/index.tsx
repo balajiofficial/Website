a1bb8e02f8020ef4429d180662a1ef963b1ac1b3
Balaji K <balajikyadav.official@gmail.com>
2021-05-05 16:12:28 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,30 +80,20 @@ export default function Home() {
                 <a href="mailto:notanemail@email.com">
                   <button className="bg-indigo-600 hover:bg-indigo-500 rounded-lg p-3 font-semibold h-14 hover:shadow-lg">
                     <svg
-                      version="1.1"
-                      id="Capa_1"
                       xmlns="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      className="inline-block w-10 pr-2"
-                      x="0px"
-                      fill="white"
-                      y="0px"
-                      viewBox="0 0 512 512"
-                      enableBackground="new 0 0 512 512"
-                      xmlSpace="preserve"
+                      className="h-8 w-8 inline-block"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
                     >
-                      <g>
-                        <g>
-                          <path
-                            d="M467,61H45C20.218,61,0,81.196,0,106v300c0,24.72,20.128,45,45,45h422c24.72,0,45-20.128,45-45V106
-			C512,81.28,491.872,61,467,61z M460.786,91L256.954,294.833L51.359,91H460.786z M30,399.788V112.069l144.479,143.24L30,399.788z
-			 M51.213,421l144.57-144.57l50.657,50.222c5.864,5.814,15.327,5.795,21.167-0.046L317,277.213L460.787,421H51.213z M482,399.787
-			L338.213,256L482,112.212V399.787z"
-                          />
-                        </g>
-                      </g>
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
                     </svg>
-                    <p className="inline-block text-lg align-middle">Email</p>
+                    <p className="inline-block text-lg align-middle pl-1">Email</p>
                   </button>
                 </a>
               </div>
