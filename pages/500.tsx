4b9e3f5f739737906bad76c803d5eb5928a068d7
Balaji K <balajikyadav.official@gmail.com>
2021-05-16 13:20:33 +0530

    Create 500.tsx

---
+++ pages/500.tsx
@@ -0,0 +1,33 @@
+import Layout from "../components/layout";
+import Head from "next/head";
+import Link from "next/link";
+
+export default function FiveZeroZero() {
+  return (
+    <Layout>
+      <Head>
+        <title>Error 500 | Balaji</title>
+      </Head>
+      <div className="dark:bg-gray-800 p-bottom">
+        <div className="pb-24 pt-4">
+          <p
+            className={
+              "flex justify-center align-middle pt-60 text-4xl font-extrabold text-center dark:text-gray-300"
+            }
+          >
+            Error 500 Page Not Found
+          </p>
+          <div className="pt-2 flex justify-center pb-3">
+            <Link href="/">
+              <a>
+                <p className="p-3 text-blue-600 hover:text-blue-400 text-lg dark:text-blue-400 dark:hover:text-blue-200">
+                  Return to Home Page
+                </p>
+              </a>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
