63306fed9fb8fcccfbd0c22593c8c0d880ca75a3
Balaji K <balajikyadav.official@gmail.com>
2021-05-17 12:31:29 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ["./components/*.tsx", "./pages/*.tsx"],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
