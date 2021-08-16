275e999eb7b519c177b4ee62907cc3c04d345601
Balaji K <balajikyadav.official@gmail.com>
2021-08-17 07:03:35 +1400

    Add device type

--- components/footer.tsx
+++ components/footer.tsx
@@ -18,7 +18,43 @@ export default class Footer extends Component {
         document.getElementById("footer").innerHTML +=
           "<a class=hover:underline href=mailto:notanemail@email.com>Email</a>";
       });
+
+    const device = this.getDeviceType();
+    if (device == "Desktop") {
+      window.document.getElementById(
+        "device"
+      ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 inline-block mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" /></svg><p class="inline-block align-middle">Desktop</p>`;
+    } else if (device == "Mobile") {
+      window.document.getElementById(
+        "device"
+      ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 inline-block mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.2" d="M12 18h.01M7 21h10a2 2 0 002-2V5a2 2 0 00-2-2H7a2 2 0 00-2 2v14a2 2 0 002 2z" /></svg><p class="inline-block align-middle">Mobile</p>`;
+    } else {
+      window.document.getElementById(
+        "device"
+      ).innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 inline-block mr-1" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tablet"><rect x="4" y="2" width="16" height="20" rx="2" ry="2"></rect><line x1="12" y1="18" x2="12.01" y2="18"></line></svg><p class="inline-block align-middle">Tablet</p>`;
     }
+  }
+
+  // From https://abdessalam.dev/blog/detect-device-type-javascript/
+  getDeviceType = (): string => {
+    const user_agent = window.navigator.userAgent;
+    if (
+      RegExp("(tablet|ipad|playbook|silk)|(android(?!.*mobi))", "i").test(
+        user_agent
+      )
+    ) {
+      return "Tablet";
+    }
+    if (
+      RegExp(
+        "Mobile|iP(hone|od)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)"
+      ).test(user_agent)
+    ) {
+      return "Mobile";
+    }
+    return "Desktop";
+  };
+
   render(): ReactElement<HTMLDivElement> {
     return (
       <div>
@@ -28,15 +64,14 @@ export default class Footer extends Component {
               <div>
                 <p id="device"></p>
               </div>
-              <h1 className="md:inline-block">
-                Copyright © 2021 Balaji K |{" "}
+              <h1>
                 <a
                   href="https://github.com/balajiofficial/Website/blob/main/LICENSE"
                   target="_blank"
-                  className="underline md:inline-block hover:no-underline"
+                  className="underline hover:no-underline"
                   rel="noreferrer"
                 >
-                  License
+                  Copyright © 2021 Balaji K
                 </a>
               </h1>
               <p id="footer" className="md:inline-block"></p>
