6fafb718b0dddf8086aabbd0f94b073e615e62bd
Balaji K <balajikyadav.official@gmail.com>
2022-05-11 15:11:57 +0530

    change links on front page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import { ReactElement, ReactFragment } from "react";
 import PageSEO from "../components/seo";
+import Image from "next/image";
 import IndexLayout from "../layouts/indexLayout";
 
 export default function Home(): ReactElement<ReactFragment> {
@@ -44,46 +46,58 @@ export default function Home(): ReactElement<ReactFragment> {
                   </a>
                 </div>
                 <div className="pt-3 pb-3">
-                  <button
-                    className="bg-blue-500 hover:bg-blue-600 dark:hover:bg-blue-400 dark:hover:shadow-gray-600 rounded-lg p-3 font-semibold h-14 hover:shadow-xl hover:-translate-y-1 transform transition-all duration-150"
-                    onClick={() => {
-                      alert("No Twitter account created yet");
-                    }}
+                  <a
+                    href="https://www.linkedin.com/in/balaji-k-0389a8239/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-white"
                   >
-                    <div className="whitespace-nowrap">
-                      <svg
-                        viewBox="0 0 24 24"
-                        className="fill-current inline-block w-10 pr-1"
-                      >
-                        <g>
-                          <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path>
-                        </g>
-                      </svg>
-                      <p className="inline-block text-lg align-middle">
-                        Twitter
-                      </p>
-                    </div>
-                  </button>
+                    <button className="bg-blue-600 hover:bg-blue-700 dark:hover:bg-blue-500 dark:hover:shadow-gray-600 rounded-lg p-3 font-semibold h-14 hover:shadow-xl hover:-translate-y-1 transform transition-all duration-150">
+                      <div className="whitespace-nowrap">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 34 34"
+                          className="w-8 h-8 inline-block mr-1.5"
+                        >
+                          <g>
+                            <path
+                              d="M34,2.5v29A2.5,2.5,0,0,1,31.5,34H2.5A2.5,2.5,0,0,1,0,31.5V2.5A2.5,2.5,0,0,1,2.5,0h29A2.5,2.5,0,0,1,34,2.5ZM10,13H5V29h5Zm.45-5.5A2.88,2.88,0,0,0,7.59,4.6H7.5a2.9,2.9,0,0,0,0,5.8h0a2.88,2.88,0,0,0,2.95-2.81ZM29,19.28c0-4.81-3.06-6.68-6.1-6.68a5.7,5.7,0,0,0-5.06,2.58H17.7V13H13V29h5V20.49a3.32,3.32,0,0,1,3-3.58h.19c1.59,0,2.77,1,2.77,3.52V29h5Z"
+                              fill="#ffffff"
+                            ></path>
+                          </g>
+                        </svg>
+                        <p className="inline-block text-lg align-middle">
+                          LinkedIn
+                        </p>
+                      </div>
+                    </button>
+                  </a>
                 </div>
                 <div className="pt-3 pb-3">
-                  <button
-                    className="bg-gradient-to-tl from-red-600 via-purple-600 to-blue-600 rounded-lg p-3 dark:hover:from-red-500 dark:hover:via-purple-500 dark:hover:to-blue-500 hover:from-red-700 hover:via-purple-700 hover:to-blue-700 dark:hover:shadow-gray-600 font-semibold h-14 hover:shadow-xl hover:-translate-y-1 transform transition-all duration-150"
-                    onClick={() => {
-                      alert("No Instagram account created yet");
-                    }}
+                  <a
+                    href="https://data.typeracer.com/pit/profile?user=balawastaken"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-white"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      className="fill-current inline-block h-8 w-8"
-                    >
-                      <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
-                    </svg>
-
-                    <p className="inline-block text-lg align-middle ml-1.5">
-                      Instagram
-                    </p>
-                  </button>
+                    <button className="bg-blue-800 hover:bg-blue-700 rounded-lg p-3 dark:hover:shadow-gray-600 font-semibold h-14 hover:shadow-xl hover:-translate-y-1 transform transition-all duration-150">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-9 w-9 inline-block"
+                        viewBox="0 0 20 20"
+                        fill="currentColor"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                      <p className="inline-block text-lg align-middle ml-1.5">
+                        TypeRacer
+                      </p>
+                    </button>
+                  </a>
                 </div>
                 <div className="pt-3 pb-3">
                   <button
